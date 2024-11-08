837d3495bc6e3df46802e585c3aa6f45fa89eb4d
Arthur Bernardo <arthur.bernardo@valori.com.vc>
2024-11-08 17:18:29 -0300

    making it rven more dynamic and adding a registry

--- fakeApi/index.js
+++ fakeApi/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/fakeApi', (req, res, next) => {
     res.send('hello from fake api!');
 });
-app.get('/mantisApi', (req, res, next) => {
+app.post('/mantisApi', (req, res, next) => {
     res.send('mantis says hello');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const registry = require('./registry.json');
+const {response} = require("express");
 
-router.all('/:apiName', (req, res) => {
-    axios.get(`http://localhost:3001/${req.params.apiName}`).then((response) => {
-        res.send(response.data)
-    });
+router.all('/:apiName/:path', (req, res) => {
+    registry.services[req.params.apiName] ?
+        axios({
+            method: req.method,
+            url: `${registry.services[req.params.apiName].url}${req.params.path}`,
+            data: req.body
+        }).then((response) => {
+            res.send(response.data)
+        })
+    : res.send("Api Name doesn't exist");
 })
 
 module.exports = router;
