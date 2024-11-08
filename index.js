044ec3eaa0f8f3b42f510b222a6882043d2ee738
Arthur Bernardo <arthur.bernardo@valori.com.vc>
2024-11-08 16:26:55 -0300

    routes creation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 const PORT = 3000;
 
 app.use(express.json());
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`Gateway live on port ${PORT}`);
